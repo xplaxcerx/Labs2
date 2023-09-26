b5204aab2389bd972b92be907acb576e722cd0e8
xplaxcerx <115776678+xplaxcerx@users.noreply.github.com>
2023-09-27 02:46:09 +0500

    Add files via upload

---
+++ evil-labs_c--master/Client/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO.Pipes;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace Client
+{
+    class Program
+    {
+        public struct Ad
+        {
+            public int X;
+            public int Y;
+            public bool Podtv;
+        }
+
+        static async Task Main()
+        {
+            Console.WriteLine("Соединяю с сервером...\n");
+
+            using (var clientPipe = new NamedPipeClientStream(".", "tonel", PipeDirection.InOut, PipeOptions.Asynchronous))
+            {
+                await clientPipe.ConnectAsync();
+
+                Console.WriteLine("Соединение установлено, отправляем данные...\n");
+
+                Ad dataToSend = new Ad
+                {
+                    X = 42,
+                    Y = 24,
+                    Podtv = false
+                };
+
+                SendData(clientPipe, dataToSend);
+
+                Console.WriteLine($"Отправлены данные: X={dataToSend.X}, Y={dataToSend.Y}, Podtv={dataToSend.Podtv}\n");
+
+                byte[] buffer = new byte[Marshal.SizeOf<Ad>()];
+                await clientPipe.ReadAsync(buffer, 0, buffer.Length);
+
+                Ad receivedData = MemoryMarshal.Read<Ad>(buffer);
+
+                Console.WriteLine($"Получены данные от сервера: X={receivedData.X}, Y={receivedData.Y}, Podtv={receivedData.Podtv}\n");
+            }
+
+            Console.WriteLine("Клиент завершил работу.");
+        }
+
+        static void SendData(NamedPipeClientStream pipe, Ad data)
+        {
+            byte[] buffer = new byte[Marshal.SizeOf<Ad>()];
+            MemoryMarshal.Write(buffer, ref data);
+            pipe.Write(buffer, 0, buffer.Length);
+        }
+    }
+}
---
+++ evil-labs_c--master/Server/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO.Pipes;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace Server
+{
+    class Program
+    {
+        public struct Ad
+        {
+            public int X;
+            public int Y;
+            public bool Podtv;
+        }
+        static async Task Main()
+        {
+            Console.WriteLine("Ожидание клиента...\n");
+
+            using (var serverPipe = new NamedPipeServerStream("tonel", PipeDirection.InOut, NamedPipeServerStream.MaxAllowedServerInstances, PipeTransmissionMode.Byte, PipeOptions.Asynchronous))
+            {
+                await serverPipe.WaitForConnectionAsync();
+
+                Console.WriteLine("Клиент подключен.\n");
+
+                Ad receivedData = ReceiveData(serverPipe);
+                Console.WriteLine($"Получены данные от клиента: X={receivedData.X}, Y={receivedData.Y}, Podtv={receivedData.Podtv}\n");
+
+                receivedData.X += receivedData.Y;
+                receivedData.Y -= receivedData.X;
+                receivedData.Podtv = true;
+
+                Console.WriteLine($"Отправляем данные клиенту: X={receivedData.X}, Y={receivedData.Y}, Podtv={receivedData.Podtv}\n");
+                SendData(serverPipe, receivedData);
+
+                await WaitForPipeDrainAsync(serverPipe);
+            }
+
+            Console.WriteLine("Сервер завершил работу.");
+        }
+        static Ad ReceiveData(NamedPipeServerStream pipe)
+        {
+            byte[] buffer = new byte[Marshal.SizeOf<Ad>()];
+            pipe.Read(buffer, 0, buffer.Length);
+            return MemoryMarshal.Read<Ad>(buffer);
+        }
+
+        static void SendData(NamedPipeServerStream pipe, Ad data)
+        {
+            byte[] buffer = new byte[Marshal.SizeOf<Ad>()];
+            MemoryMarshal.Write(buffer, ref data);
+            pipe.Write(buffer, 0, buffer.Length);
+        }
+
+        static async Task WaitForPipeDrainAsync(NamedPipeServerStream pipe)
+        {
+            var semaphore = new SemaphoreSlim(1);
+            await semaphore.WaitAsync();
+            semaphore.Release();
+        }
+    }
+}
