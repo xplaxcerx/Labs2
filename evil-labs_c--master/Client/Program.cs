ba915d5ae505f71e3dd851198d5f574be2adee56
xplaxcerx <115776678+xplaxcerx@users.noreply.github.com>
2023-10-02 15:29:06 +0500

    Update Program.cs

--- evil-labs_c--master/Client/Program.cs
+++ evil-labs_c--master/Client/Program.cs
@@ -6,26 +6,31 @@ class Program
 {
     static void Main()
     {
-        using (var serverPipe = new NamedPipeServerStream("MyPipe", PipeDirection.InOut))
+        using (var clientPipe = new NamedPipeClientStream(".", "MyPipe", PipeDirection.InOut))
         {
-            Console.WriteLine("Сервер ожидает подключения...");
-            serverPipe.WaitForConnection();
+            Console.WriteLine("Подключение к серверу...");
+            clientPipe.Connect();
 
             var formatter = new BinaryFormatter();
 
             while (true)
             {
-        
+                
+                MyData requestData = new MyData { Number = 10, Message = "Запрос от клиента" };
+                var memoryStream = new System.IO.MemoryStream();
+                formatter.Serialize(memoryStream, requestData);
+                clientPipe.Write(memoryStream.GetBuffer(), 0, (int)memoryStream.Length);
+                Console.WriteLine("Отправлено серверу.");
+
+                
                 byte[] buffer = new byte[1024];
-                int bytesRead = serverPipe.Read(buffer, 0, buffer.Length);
-                var memoryStream = new System.IO.MemoryStream(buffer, 0, bytesRead);
-                MyData receivedData = (MyData)formatter.Deserialize(memoryStream);
-                Console.WriteLine($"Получено от клиента: Number = {receivedData.Number}, Message = {receivedData.Message}");
-                MyData responseData = new MyData { Number = 42, Message = "Ответ от сервера" };
-                memoryStream = new System.IO.MemoryStream();
-                formatter.Serialize(memoryStream, responseData);
-                serverPipe.Write(memoryStream.GetBuffer(), 0, (int)memoryStream.Length);
-                Console.WriteLine("Отправлено клиенту.");
+                int bytesRead = clientPipe.Read(buffer, 0, buffer.Length);
+                memoryStream = new System.IO.MemoryStream(buffer, 0, bytesRead);
+                MyData response = (MyData)formatter.Deserialize(memoryStream);
+                Console.WriteLine($"Получено от сервера: Number = {response.Number}, Message = {response.Message}");
+
+                
+                System.Threading.Thread.Sleep(2000);
             }
         }
     }
