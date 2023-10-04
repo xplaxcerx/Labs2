355e3f753557119b5bdc4a0e9391d8a2bd60046e
xplaxcerx <115776678+xplaxcerx@users.noreply.github.com>
2023-10-04 13:59:23 +0500

    Update Program.cs

--- LR1/Client/Program.cs
+++ LR1/Client/Program.cs
@@ -1,32 +1,56 @@
 using System;
 using System.IO.Pipes;
-using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
-class Program
+namespace Client
 {
-    static void Main()
+    class Program
     {
-        using (var clientPipe = new NamedPipeClientStream(".", "MyPipe", PipeDirection.InOut))
+        public struct Ad
         {
-            Console.WriteLine("Подключение к серверу...");
-            clientPipe.Connect();
+            public int X;
+            public int Y;
+            public bool Podtv;
+        }
 
-            var formatter = new BinaryFormatter();
+        static async Task Main()
+        {
+            Console.WriteLine("Соединяю с сервером...\n");
 
-            while (true)
+            using (var clientPipe = new NamedPipeClientStream(".", "tonel", PipeDirection.InOut, PipeOptions.Asynchronous))
             {
-                
-                MyData requestData = new MyData { Number = 10, Message = "Запрос от клиента" };
-                formatter.Serialize(clientPipe, requestData);
-                Console.WriteLine("Отправлено серверу.");
+                await clientPipe.ConnectAsync();
 
-                
-                MyData response = (MyData)formatter.Deserialize(clientPipe);
-                Console.WriteLine($"Получено от сервера: Number = {response.Number}, Message = {response.Message}");
+                Console.WriteLine("Соединение установлено, отправляем данные...\n");
 
-                
-                System.Threading.Thread.Sleep(2000);
+                Ad dataToSend = new Ad
+                {
+                    X = 42,
+                    Y = 24,
+                    Podtv = false
+                };
+
+                SendData(clientPipe, dataToSend);
+
+                Console.WriteLine($"Отправлены данные: X={dataToSend.X}, Y={dataToSend.Y}, Podtv={dataToSend.Podtv}\n");
+
+                byte[] buffer = new byte[Marshal.SizeOf<Ad>()];
+                await clientPipe.ReadAsync(buffer, 0, buffer.Length);
+
+                Ad receivedData = MemoryMarshal.Read<Ad>(buffer);
+
+                Console.WriteLine($"Получены данные от сервера: X={receivedData.X}, Y={receivedData.Y}, Podtv={receivedData.Podtv}\n");
             }
+
+            Console.WriteLine("Клиент завершил работу.");
+        }
+
+        static void SendData(NamedPipeClientStream pipe, Ad data)
+        {
+            byte[] buffer = new byte[Marshal.SizeOf<Ad>()];
+            MemoryMarshal.Write(buffer, ref data);
+            pipe.Write(buffer, 0, buffer.Length);
         }
     }
 }
