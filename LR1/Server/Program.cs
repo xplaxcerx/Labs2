ccec1f06080dc1d002acf16a4e2ce0e968ea9fe3
xplaxcerx <115776678+xplaxcerx@users.noreply.github.com>
2023-12-17 23:49:10 +0500

    Update Program.cs

--- LR1/Server/Program.cs
+++ LR1/Server/Program.cs
@@ -1,136 +1,41 @@
-using System;
-using System.Collections.Generic;
-using System.IO.Pipes;
+﻿﻿﻿using System.IO.Pipes;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Threading;
-using System.Threading.Tasks;
 
-namespace Server
+namespace Client;
+class Program
 {
     public struct Ad
     {
         public int X;
         public bool Podtv;
-
-        public override string ToString() => $"Данные = {X}, Ответ = {Podtv}";
     }
-
-    internal class Program
+    public static void Main()
     {
-        private static readonly CancellationTokenSource Up = new CancellationTokenSource();
-        private static readonly CancellationToken Token = Up.Token;
-        private static readonly PriorityQueue<Ad, int> Queue = new PriorityQueue<Ad, int>();
-        private static readonly Mutex Mutex = new Mutex();
-
-        private static Task ClientTask(CancellationToken token)
+        try
         {
-            return Task.Run(() =>
+            Console.WriteLine("Соеденение с базой...\n");
+            var stream = new NamedPipeClientStream(".", "tonel", PipeDirection.InOut);
+            stream.Connect();
+            Console.WriteLine("Соеденено \n");
+            Console.WriteLine("Ожидание данных... \n");
+            while (true)
             {
-                while (!token.IsCancellationRequested)
-                {
-                    Console.WriteLine("Введите значение: ");
-                    var value = Console.ReadLine();
-                    if (value.Length == 0)
-                    {
-                        Console.WriteLine("Вы не ввели цифры, попробуйте снова: \n");
-                        continue;
-                    }
+                byte[] array = new byte[Unsafe.SizeOf<Ad>()];
+                stream.Read(array);
+                var answer = MemoryMarshal.Read<Ad>(array);
 
-                    Console.WriteLine("Введите приоритет: ");
-                    var priority = Console.ReadLine();
-                    if (priority.Length == 0)
-                    {
-                        Console.WriteLine("Вы не ввели цифры, попробуйте снова: \n");
-                        continue;
-                    }
-                    var data = new Ad { X = Convert.ToInt32(value), Podtv = false };
+                Console.WriteLine($"Получил: {answer.X}, {answer.Podtv}\n");
+                answer.Podtv = true;
+                Console.WriteLine($"Отправил {answer.X}, {answer.Podtv}...\n");
 
-                    lock (Mutex)
-                    {
-                        Queue.Enqueue(data, Convert.ToInt32(priority));
-                    }
-                }
-            });
-        }
-
-        private static Task ServerTask(NamedPipeServerStream stream, CancellationToken token)
-        {
-            return Task.Run(() =>
-            {
-                List<Ad> uds = new List<Ad>();
-                try
-                {
-                    while (!token.IsCancellationRequested)
-                    {
-                        if (Queue.Count >= 1)
-                        {
-                        Mutex.WaitOne();
-                        var data = Queue.Dequeue();
-                        mutex.ReleaseMutex();
-                        byte[] spam = new byte[Unsafe.SizeOf<Ad>()];
-                        MemoryMarshal.Write<Ad>(spam, ref data);
-                        stream.Write(spam);
-                        byte[] array = new byte[Unsafe.SizeOf<Ad>()];
-                        stream.Read(array);
-                        uds.Add(MemoryMarshal.Read<Ad>(array));
-                    }
-                            
-                        }
-                    }
-                }
-                catch (IOException ex)
-                {
-                    Console.WriteLine($"IOException in ServerTask: {ex.Message}");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"An unexpected error occurred in ServerTask: {ex.Message}");
-                }
-                finally
-                {
-                    stream.Dispose(); 
-                }
-
-                foreach (var item in uds)
-                {
-                    Console.WriteLine(item);
-                }
-            });
-        }
-
-        private static async Task Main(string[] args)
-        {
-            Console.CancelKeyPress += (s, e) =>
-            {
-                e.Cancel = true;
-                Up.Cancel();
-            };
-
-            try
-            {
-                using (var stream = new NamedPipeServerStream("tonel", PipeDirection.InOut))
-                {
-                    stream.WaitForConnection();
-                    Console.WriteLine("Клиент подключен\n");
-                    Task task1 = ServerTask(stream, Token);
-                    Task task2 = ClientTask(Token);
-                    await Task.WhenAll(task1, task2);
-                }
-            }
-            catch (IOException ex)
-            {
-                Console.WriteLine($"IOException in Main: {ex.Message}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"An unexpected error occurred in Main: {ex.Message}");
-            }
-            finally
-            {
-                Up.Dispose();  
+                byte[] spam = new byte[Unsafe.SizeOf<Ad>()];
+                MemoryMarshal.Write<Ad>(spam, ref answer);
+                stream.Write(spam);
             }
         }
-    }
+        catch
+        {
+        }
+    }   
 }
-#An unexpected error occurred in ServerTask: Index was outside the bounds of the array. 
