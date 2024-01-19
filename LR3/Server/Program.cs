a50608f32befce23c5671e58c5a8ea59b2212f21
xplaxcerx <115776678+xplaxcerx@users.noreply.github.com>
2024-01-19 15:32:53 +0500

    Update Program.cs

--- LR3/Server/Program.cs
+++ LR3/Server/Program.cs
@@ -1,4 +1,5 @@
-﻿﻿using System;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Pipes;
@@ -7,16 +8,76 @@ using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
 
-public struct Ad
+public struct ClientData
 {
     public double A;
     public double B;
 }
 
-public struct Ud
+public struct ServerData
 {
     public double Result;
     public override string ToString() => $"Res = {Result}";
+}
+
+public class PriorityQueue<TItem, TPrior> where TPrior : class
+{
+    private readonly SortedDictionary<TPrior, Queue<TItem>> _dictionary = new SortedDictionary<TPrior, Queue<TItem>>();
+    private readonly object _lockObject = new object();
+
+    public int Count
+    {
+        get
+        {
+            lock (_lockObject)
+            {
+                return _dictionary.Values.Sum(queue => queue.Count);
+            }
+        }
+    }
+
+    public void Enqueue(TItem item, TPrior priority)
+    {
+        lock (_lockObject)
+        {
+            if (!_dictionary.TryGetValue(priority, out var queue))
+            {
+                queue = new Queue<TItem>();
+                _dictionary.Add(priority, queue);
+            }
+            queue.Enqueue(item);
+        }
+    }
+
+    public TItem Dequeue()
+    {
+        lock (_lockObject)
+        {
+            if (_dictionary.Count == 0)
+            {
+                // Queue is empty, handle accordingly (throw an exception or return a default value)
+                throw new InvalidOperationException("Queue is empty.");
+            }
+
+            var queue = _dictionary.Values.First();
+
+            if (queue.Count == 0)
+            {
+                // If the current queue is empty, remove it from the dictionary
+                _dictionary.Remove(_dictionary.First().Key);
+
+                // If the dictionary is now empty, handle accordingly (throw an exception or return a default value)
+                if (_dictionary.Count == 0)
+                {
+                    throw new InvalidOperationException("Queue is empty.");
+                }
+            }
+
+            var item = queue.Dequeue();
+            return item;
+        }
+    }
+
 }
 
 internal class Program
@@ -24,7 +85,7 @@ internal class Program
     private static int id = 0;
     static CancellationTokenSource up = new CancellationTokenSource();
     static CancellationToken token = up.Token;
-    static PriorityQueue<Ad, string> queue = new PriorityQueue<Ad, string>();
+    static PriorityQueue<ClientData, string> queue = new PriorityQueue<ClientData, string>();
     static Mutex mutex = new Mutex();
 
     private static Task clientTask(CancellationToken token)
@@ -33,7 +94,7 @@ internal class Program
         {
             while (!token.IsCancellationRequested)
             {
-                var data = new Ad();
+                var data = new ClientData();
                 Console.WriteLine($"Введите значение A -> ");
                 var nach = Console.ReadLine();
                 if (!double.TryParse(nach, out double valueA))
@@ -56,7 +117,6 @@ internal class Program
                 {
                     data.B = valueB;
                 }
-
                 queue.Enqueue(data, Guid.NewGuid().ToString()); // Using Guid as a non-nullable string
             }
         });
@@ -66,12 +126,12 @@ internal class Program
     {
         return Task.Run(() =>
         {
-            List<Ud> uds = new List<Ud>();
+            List<ServerData> uds = new List<ServerData>();
             while (!token.IsCancellationRequested)
             {
                 try
                 {
-                    Ad data;
+                    ClientData data;
                     lock (mutex)
                     {
                         if (queue.Count >= 1)
@@ -85,7 +145,6 @@ internal class Program
                             continue;
                         }
                     }
-
                     Task task_3 = runclient(token, uds, data);
                 }
                 catch (Exception ex)
@@ -93,7 +152,6 @@ internal class Program
                     Console.WriteLine($"Error: {ex.Message}");
                 }
             }
-
             foreach (var item in uds)
             {
                 Console.WriteLine(item);
@@ -101,27 +159,27 @@ internal class Program
         });
     }
 
-    private static async Task runclient(CancellationToken token, List<Ud> uds, Ad data)
+    private static async Task runclient(CancellationToken token, List<ServerData> uds, ClientData data)
     {
         id++;
         string name = $"tonel_{id}";
         using (Process myProcess = new Process())
         {
-            myProcess.StartInfo.FileName = "C:\\Users\\egoro\\Desktop\\LR3\\Client\\bin\\Debug\\net7.0\\Client.exe";
+            myProcess.StartInfo.FileName = "C:\\Users\\egoro\\Desktop\\lab-3n\\Client\\bin\\Debug\\net7.0\\Client.exe";
             myProcess.StartInfo.Arguments = name;
             myProcess.Start();
 
             var stream = new NamedPipeServerStream($"{name}", PipeDirection.InOut);
             await stream.WaitForConnectionAsync();
 
-            byte[] spam = new byte[Unsafe.SizeOf<Ad>()];
+            byte[] spam = new byte[Unsafe.SizeOf<ClientData>()];
             MemoryMarshal.Write(spam, ref data);
             await stream.WriteAsync(spam, token);
 
-            byte[] array = new byte[Unsafe.SizeOf<Ud>()];
+            byte[] array = new byte[Unsafe.SizeOf<ServerData>()];
             await stream.ReadAsync(array, token);
 
-            uds.Add(MemoryMarshal.Read<Ud>(array));
+            uds.Add(MemoryMarshal.Read<ServerData>(array));
 
             await Task.Run(() => myProcess.WaitForExit());
         }
